1b02acfdd891c575ecde34bde08f8adb9d014b8f
tonyeyannelu1-rgb <tonyeyannelu1@gmail.com>
2026-07-19 14:04:43 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,5 @@
 api/generate.js
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Méthode non autorisée' });
   }
